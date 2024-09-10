217dd10b887489c305aa5bf6fe45a73445f7adf8
Valdi <rvdans002@gmail.com>
2024-09-10 16:28:03 +0700

    add see more button

--- src/components/index/FeaturedProjects.tsx
+++ src/components/index/FeaturedProjects.tsx
@@ -1,3 +1,6 @@
+import { Link } from '@nextui-org/link';
+import { Button } from '@nextui-org/button';
+
 import projectsContent from '@/content/index/projects.json';
 import ProjectItem from '../ProjectItem';
 
@@ -6,10 +9,21 @@ const FeaturedProjects = () => {
     <section id='projects' className='min-h-screen pt-4 scroll-mt-16'>
       <h2 className='text-5xl font-extrabold'>{projectsContent.title}</h2>
 
-      <div className='flex flex-col gap-8 my-24'>
-        {projectsContent.projects.map((project, idx) => (
-          <ProjectItem key={idx} {...project} />
-        ))}
+      <div className='flex flex-col items-center gap-8 my-24'>
+        <div className='flex flex-col gap-8'>
+          {projectsContent.projects.map((project, idx) => (
+            <ProjectItem key={idx} {...project} />
+          ))}
+        </div>
+
+        <Button
+          href='https://github.com/ValdiANS'
+          as={Link}
+          showAnchorIcon
+          isExternal
+        >
+          See more
+        </Button>
       </div>
     </section>
   );
