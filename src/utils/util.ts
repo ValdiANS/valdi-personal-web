891de00419660cd57af756e2a29fc16663e010cb
Valdi <rvdans002@gmail.com>
2024-10-08 13:38:44 +0700

    create util.ts and add ICON_MAPPER in it

---
+++ src/utils/util.ts
@@ -0,0 +1,10 @@
+const ICON_MAPPER: {
+  [key: string]: string;
+} = {
+  website: 'ph:globe-light',
+  github: 'logos:github-icon',
+  gitlab: 'logos:gitlab',
+  lock: 'material-symbols:lock-outline',
+};
+
+export { ICON_MAPPER };
